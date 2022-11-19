ccf44a6feacd8d5c5f892e672992ec3239c3c913
htay <htay.dev@gmail.com>
2022-11-19 17:02:54 +0630

    add letter_for_

--- Day-24(Mail+Merge+Project+Start)/main.py
+++ Day-24(Mail+Merge+Project+Start)/main.py
@@ -6,7 +6,7 @@ with open("./Input/Names/invited_names.txt") as nm:
     names = nm.readlines()
     for i in range(len(names)):
         names[i] = names[i].replace("\n", "")
-        with open(f"./Output/ReadyToSend/{names[i]}.txt", "w") as op:
+        with open(f"./Output/ReadyToSend/letter_for_{names[i]}.txt", "w") as op:
             text = starting_letter.replace("[name]", f"{names[i]}")
             op.write(text)
 
